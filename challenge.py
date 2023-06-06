a3e413c3a60f31da2f26742c737ad88ee1b3c4d3
Siyao Xing <Siyao@Siyaos-MacBook-Pro-2.local>
2023-06-06 22:32:03 +0100

    refined functions

--- challenge.py
+++ challenge.py
@@ -1,14 +1,14 @@
 def add_un(word): #1. Function to return a word with the prefix 'un' added 
-    return print ('un'+word)
+    return 'un'+word
 
 add_un('happy')
 
-vocab_words = ['prefix', 'word', "word2"] #2. Function to return the same prefix to a group of words 
+vocab_words = ['un', 'happy', "kind"] #2. Function to return the same prefix to a group of words 
 
 def make_word_groups(vocab_words):
     i=1
     while i<len(vocab_words):
-        new = print(vocab_words[0]+vocab_words[i])
+        new = vocab_words[0]+vocab_words[i]
         i+=1
     return new 
 
@@ -16,16 +16,17 @@ make_word_groups(vocab_words) #3. Function to remove the suffix 'ness' and retur
 
 def remove_suffix_ness(word):
     if word[-5:] =='iness':
-        return print (word.replace('iness', 'y'))
+        return word.replace('iness', 'y')
     else:
-        return print(word.replace('ness', ''))
+        return word.replace('ness', '')
     
 
 remove_suffix_ness('handsomeness')
 
 def adjective_to_verb(sentence, index): #4. Function to make any adjective in a sentence into a verb
     new_sentence = sentence.split(" ")
-    return print(new_sentence[index]+'en')
+    return new_sentence[index].strip(".,?!'\")()") +'en'
 
 adjective_to_verb('It is too bright', -1)
+
         
